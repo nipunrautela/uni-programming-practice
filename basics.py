e7c164333a34ac1e9ec79364fafc761e5344e8fb
NipunRautela <nipunrautela261@gmail.com>
2020-09-30 16:48:27 +0530

    First commit. No message

---
+++ basics.py
@@ -0,0 +1,16 @@
+print("Welcome to python")
+
+a = 5
+b = 10
+c = a + b
+print(c)
+
+x = input("Enter first number : ")
+y = input("Enter Second number : ")
+z = int(x) + int(y)
+print(z)
+
+if c > z:
+    print("C is the largest value")
+else:
+    print("Z is the largest value")
