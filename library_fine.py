93759e1a40dffddfe170b36f1c48cf573d114a5b
NipunRautela <nipunrautela261@gmail.com>
2020-10-12 15:29:40 +0530

    Added library_fine.py file as well

---
+++ library_fine.py
@@ -0,0 +1,15 @@
+de = int(input("Enter the expected date of return"))
+me = int(input("Enter the expected date of return"))
+ye = int(input("Enter the expected date of return"))
+da = int(input("Enter the actual date of return"))
+ma = int(input("Enter the actual date of return"))
+ya = int(input("Enter the actual date of return"))
+
+if ye != ya:
+    print("Fine:", 10000)
+elif me != ma:
+    print("Fine:", (ma-me)*500)
+elif de != da:
+    print("Fine:", (da-de)*15)
+else:
+    print("No fine.")
