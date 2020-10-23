69b956e824a96347035b3523faa77950e9fe1ae0
NipunRautela <nipunrautela261@gmail.com>
2020-10-23 15:53:52 +0530

    Added stopwords counter, and customer region

---
+++ CSE1001/strings/customer_region.py
@@ -0,0 +1,16 @@
+customer_info = []
+customer_count = int(input("Enter the number of customers: "))
+
+for i in range(customer_count):
+    customer_info.append([
+        input("Enter name of customer {0}: ".format(i+1)),
+        int(input("Enter age of customer {0}: ".format(i+1))),
+        input("Enter gender of customer {0}: ".format(i+1)),
+        input("Enter region of customer {0}: ".format(i+1)),
+    ])
+region = input("Enter the region to check customers: ")
+for customer in customer_info:
+    if customer[3] == region:
+        print("Customer Name:", customer[0])
+        print("Customer Age:", customer[1])
+        print("Customer Gender:", customer[2], end="\n\n")
