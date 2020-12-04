d03757a7685b027ba2977c55afc3a34489dc4bed
NipunRautela <nipunrautela261@gmail.com>
2020-12-04 14:26:01 +0530

    Changed directory structure

--- CSE1001/Python/file handling/writing.py
+++ CSE1001/Python/file handling/writing.py
@@ -1,6 +1,6 @@
 fo = open("./CSE1001/storage/foo.txt", "w")
 
-fo.write("Hello! Welcome to Python Files.")
+fo.write("Hello! This is a file.\n")
 
 print("File Name: ", fo.name)
 print("Is Closed? ", fo.closed)
@@ -8,6 +8,11 @@ print("File Mode: ", fo.mode)
 
 fo.close()
 print(fo.closed)
+
+
+fo = open("./CSE1001/storage/foo.txt", "a")
+fo.write("Writing to file goes brrrr")
+fo.close()
 
 fo = open("./CSE1001/storage/foo.txt", "r")
 print(fo.read())
